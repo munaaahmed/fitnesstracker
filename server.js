c496631e8ec1e81ba455117168605698cda6656f
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-12-18 19:15:18 -0800

    final

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb+srv://muna:hello@cluster0.zodlu.mongodb.net/muna?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
